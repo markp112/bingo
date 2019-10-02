510ebe1a9168cd90eb7307c7e7310add6be982f8
Mark Phillips <mark.phillips1965@googlemail.com>
2019-10-02 14:21:30 +0100

    working on boards

--- game.js
+++ game.js
@@ -37,7 +37,7 @@ const initBingoNumbers = () => {
         
         bingoNumbers.push(bingoNumber);
     }
-    console.log(bingoNumbers);
+    console.log("bingonumbers=",bingoNumbers);
 };
 
 // generate a random number between min and max
@@ -46,46 +46,45 @@ const getRandom =(min, max) => {
 }
 
 const getUniqueRandom = (min,max) => {
+
     let isUnique = false;
     let uniqueNumber = -1;
+
     while (!isUnique){
         uniqueNumber = getRandom(min,max);
-        if(!bingoNumbers[uniqueNumber].isPicked) isUnique = true;
+        
+        console.log("TCL: getUniqueRandom -> uniqueNumber", uniqueNumber)
+        console.log("TCL: getUniqueRandom -> bingoNumbers", bingoNumbers);
+        console.log(
+            "TCL: getUniqueRandom -> bingoNumbers[uniqueNumber]",
+            bingoNumbers[uniqueNumber]
+        );
+        if(!bingoNumbers[(uniqueNumber-1)].isPicked) isUnique = true;
+      
     }
     return uniqueNumber;
 }
 
 
-// fills a row with a combination of numbers and blank spaces
-// takes in a parameter of row
-const populateRow = (row) => {
+// get unique number for the cell that has not been used before
+const getNumberForCell = col => {
     
-    for (let col = 0; col < 9; col++){
-        // each column will have a number as follows
-        // col1 1-9, col2 10-19, col3 20-29 etc
-        let rand = getUniqueRandom((col * 10) + 1, (col * 10) + 9);
+    let rand = getUniqueRandom(col * 10 + 1, col * 10 + 9);
+    return rand;
+};
 
-        let colElement = row.children[col];
-
-        console.log(colElement)
-       
-
-
-    }
-
-
-}
-
-// purpose of this function is to populate a game card with 90
-// numbers 0 -90 across the 27 cells on each card
-// takes in a reference to a card
-const initCard = (playerCard) => {
-    let rows = getRowElements(playerCard);
+const populateCell = (cell,indexCol) => {
     
-    for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
-        let row = rows[rowIndex];
-        populateRow(row);
-    }
+    let cellNumber = getNumberForCell(indexCol);
+
+    cell.innerText = cellNumber;
+    console.log("TCL: populateCell -> cellNumber", cellNumber, indexCol)
+
+    console.log(bingoNumbers[cellNumber]);
+    // console.log("TCL: populateCell -> bingoNumbers[cellNumber]", bingoNumbers[cellNumber])
+    bingoNumbers[cellNumber - 1].isPicked = true;
+
+
 }
 
 // return all the rows for a player card
@@ -96,6 +95,24 @@ const getRowElements = cardToGet => {
     return rows;
 };
 
+// purpose of this function is to populate a game card with 90
+// numbers 0 -90 across the 27 cells on each card
+// takes in a reference to a card
+const initCard = (playerCard) => {
+    let rows = getRowElements(playerCard);
+
+    console.log("TCL: initCard -> rows", rows)
+    $(rows).each ((indexRow, row) => {
+        
+        $( $(row)[0].children).each((indexCol, cell) => {
+ 
+            populateCell(cell, indexCol);
+        })
+    })
+}
+
+
+
 const getRow = (rows, index) => {
     return rows[index];
 }
@@ -103,5 +120,7 @@ const getRow = (rows, index) => {
 $(document).ready(()=>{
     initBingoNumbers();
     initCard("#player-card .card-wrapper .card .row");
+
+    //populateCell("cell",1)
 
 })
